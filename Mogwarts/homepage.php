cb47e26792e50d268e71ee638fe402c369f9ea9f
kreutzer24 <lukaskreutzer2006@gmail.com>
2024-05-09 17:39:01 +0200

    r

--- Mogwarts/homepage.php
+++ Mogwarts/homepage.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link rel"stylesheet" href="Style/stylehomepage.css">
+    <link rel="stylesheet" href="../Style/stylehomepage.css">
 </head>
 <body>
+    
     <header>
     <h2 class="logo">MOGWARTS UNIVERSITY</h2>
 <nav class="navigation">
@@ -17,6 +18,5 @@
 </body>
 </html>
 <?php
-echo "suck my pp";
 //hallo
 ?>
