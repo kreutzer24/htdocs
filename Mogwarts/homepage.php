ba4b10490af4c1a0ba12aa863362d527eeac6a1d
kreutzer24 <lukaskreutzer2006@gmail.com>
2024-05-09 17:02:50 +0200

    k

--- Mogwarts/homepage.php
+++ Mogwarts/homepage.php
@@ -3,10 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home</title>
+    <link rel"stylesheet" href="stylehomepage.css">
 </head>
 <body>
-    
+    <header>
+    <h2 class="logo">MOGWARTS UNIVERSITY</h2>
+<nav class="navigation">
+    <a href="index.php">login<span></span></a>
+</nav> 
+
+    </header>
 </body>
 </html>
 <?php
