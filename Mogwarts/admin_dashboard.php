d23bd9fcdb07b00752e13798ee6b3d4534fcf9dc
kreutzer24 <lukaskreutzer2006@gmail.com>
2024-05-11 22:24:38 +0200

    k

---
+++ Mogwarts/admin_dashboard.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="../Style/styleadmin.css">
+</head>
+
+<body>
+    <header>
+        <a href=""><img src="../img/uni.png"></a>
+        <h2 class="logo">MOGWARTS UNIVERSITY</h2>
+        <nav class="navigation">
+            <a href="#">Home</a>
+            <a href="#">Users</a>
+            <a href="#">Courses</a>
+            <a href="#">Settings</a>
+            <a href="#">Logout</a>
+        </nav>
+    </header>
+
+    <div class="container">
+        <h1>Welcome, Admin!</h1>
+        <!-- Hier können Sie spezifische Inhalte für das Admin-Dashboard hinzufügen -->
+    </div>
+
+</body>
+
+</html>
---
+++ Mogwarts/dozent_dashboard.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dozent Dashboard</title>
+    <link rel="stylesheet" href="../Style/styledozent.css">
+</head>
+
+<body>
+    <header>
+        <a href=""><img src="../img/uni.png"></a>
+        <h2 class="logo">MOGWARTS UNIVERSITY</h2>
+        <nav class="navigation">
+            <a href="#">Home</a>
+            <a href="#">Courses</a>
+            <a href="#">Profile</a>
+            <a href="#">Logout</a>
+        </nav>
+    </header>
+
+    <div class="container">
+        <h1>Welcome, Dozent!</h1>
+        <!-- Here you can add content specific to the dozent dashboard -->
+    </div>
+
+</body>
+
+</html>
